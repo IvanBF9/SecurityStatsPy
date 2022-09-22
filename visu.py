ea6b8f806a130f5e088516b236ad12ffce0463d5
IvanBF9 <ivanbraga@outlook.fr>
2022-09-22 14:11:01 +0200

    upload sah

--- visu.py
+++ visu.py
@@ -1,13 +1,16 @@
+from asyncore import file_dispatcher
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
+import altair as alt
 from chomage import get_chomeurs
 
 data_chomage = get_chomeurs()
 
 fig = plt.figure()
 
-plt.plot(data_chomage['Value']) 
+plt.plot(data_chomage['Value'])
 #plt.xlim(2008, 2022)
 
 st.pyplot(fig)
+
